cf8827a20d97ae95ff155668a30ded3d7dc108ad
Ella <ekosardata@gmail.com>
2022-10-01 12:02:05 +0200

    router states

--- rc12-13_router-example-2/src/index.js
+++ rc12-13_router-example-2/src/index.js
@@ -5,3 +5,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+
+
+
+
+// nav  => ul -> li-> link to
+// logo => link to
+
+// nav=> ul -> li -> Navlink
+// navlink ve link to icin tiklamak icin link olmali.
+// navlink = link to (tek fark navlink te active lik ozelliginin olmasi)
+
+// fonksiyon(event) icinde => useNavigate yani navigate().
+// useNavigate ile bilgi de gonderileblir.
+// gonderilen bu bilgiler useLocation ile yakalanir.
+
+// useParams : linkte gonderilen parametreyi yakalamak icin 
+
+//Navigate(component olan) : component seviyesinde kullanilir
+// or: nasted route larda yonlendirme icin kullanilir.
